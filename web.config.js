c8b0161b07e8838c1fa3631ff00a5fe0973b18ca
evgen7171 <48080044+evgen7171@users.noreply.github.com>
2020-12-05 02:36:22 +0700

    1-5 задания к первому уроку, действия из методички

---
+++ web.config.js
@@ -0,0 +1,13 @@
+const path = require("path");
+const webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        app: './index.js',
+    },
+    context: path.resolve(__dirname, "src"),
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: 'app.js',
+    },
+};
